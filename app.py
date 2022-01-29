1b40524c8c448795618509c5a036429e6593a5ce
jacarcelenb <carcelenjorge17@gmail.com>
2022-01-29 17:54:17 -0500

    Cambios en el app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from flask import Flask , render_template , request , url_for
+from flask import Flask , render_template
 
 
-acc = 0.0
-rec = 0.0
-prec = 0.0
-f1 = 0.0
-r2 = 0.0
-
-import cancer_detection as mlp_cancer
 
 
 app = Flask(__name__)
@@ -17,23 +10,12 @@ def index():
 
     return render_template("index.html")
 
-@app.route('/metricas' , methods =["GET","POST"])
+@app.route('/metricas')
 
 def metricas():
-    if request.method == "POST":
-         neuronas = request.form["neuronas"]
-         epocas = request.form["epocas"]
-         optimizador = request.form["optimizador"]
-         acc , rec , prec , f1 , r2 = mlp_cancer.predict_cancer(neuronas,epocas,optimizador)
-         print(acc)
-         print(rec)
-         print(prec)
-         print(f1)
-         print(r2)
-         exac = acc
-         recall = rec
-    return render_template("metricas.html" , accuracy = exac , sensi = recall , precision = prec , f1score = f1,
-    coefr2 = r2) 
+  
+    return render_template("metricas.html") 
+
 @app.route('/info')
 
 def info():
